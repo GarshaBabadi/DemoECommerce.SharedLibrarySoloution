4911c79fac0f1bbe9aaf4818de95752bcbdef2e0
GarshaBabadi <garshababadireyhani@gmail.com>
2025-12-30 22:33:33 +0330

    Commit

--- eCommerce.SharedLibrary/Responses/Response.cs
+++ eCommerce.SharedLibrary/Responses/Response.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace eCommerce.SharedLibrary.Responses
 {
-    public record Response(bool Flag = false, string message = null!)
+    public record Response(bool Flag = false, string? Message = null)
     {
     }
 }
